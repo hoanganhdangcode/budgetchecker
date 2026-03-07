79aaca7ba0455fb8ae4f9c84ca2d5baaf1d828ed
hoanganhdangcode <ngohoanganh14062002@gmail.com>
2026-03-07 19:14:23 +0700

    fix android cd: setting fix

--- android/app/src/main/java/com/hoanganhdangcode/budgetchecker/ui/MainActivity.kt
+++ android/app/src/main/java/com/hoanganhdangcode/budgetchecker/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.hoanganhdangcode.budgetchecker
+package com.hoanganhdangcode.budgetchecker.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.hoanganhdangcode.budgetchecker.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

bad066133ca22dc74c0fe034948cb01478793903
hoanganhdangcode <ngohoanganh14062002@gmail.com>
2026-03-07 16:44:30 +0700

    first commit

---
+++ be/budgetchecker_backend/budgetchecker_backend/Program.cs
@@ -0,0 +1,74 @@
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+var service = builder.Services;
+//swagger
+service.AddEndpointsApiExplorer();
+service.AddSwaggerGen(
+    option => {
+        option.SwaggerDoc("v1", new() { Title = "Budget Checker", Version = "v1", Description = "Backend Budget Checker api swagger document" });
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "Nhập JWT Token theo định dạng: **Bearer {token}**"
+        });
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer" }
+                            }, new List<string>() }
+                    });
+    });
+
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "bugetchecker v1");
+    c.RoutePrefix = string.Empty;
+});
+
+
+
+//app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
